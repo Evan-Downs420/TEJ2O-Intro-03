b28fa19ccf167d2b2637d5e4ab27145bb763259e
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-02-04 13:23:39 -0500

    changed Hello, World! to Hello, Evan!

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan Downs
+ * Created on: feb 2026
+ * This program will print Hello, World!
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, Evan')
